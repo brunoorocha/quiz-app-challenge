505cf1a6eb65f60719e7f370f704caafca5fc6ac
Bruno Rocha <mail.brunorocha@gmail.com>
2020-01-25 09:04:30 -0300

    Created the project groups structure

---
+++ quiz-app/application/SceneDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  SceneDelegate.swift
+//  quiz-app
+//
+//  Created by Bruno Rocha on 25/01/20.
+//  Copyright © 2020 Bruno Rocha. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: scene)
+
+        window.rootViewController = ViewController()
+
+        self.window = window
+        self.window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
